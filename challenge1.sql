4c1087e4f0dd28ae62ab801baa4a3b073414f0ba
yusufbahtiarr <yusufbdev@gmail.com>
2025-06-17 16:20:05 +0700

    feat: add minitask no 6 and 7

--- challenge1.sql
+++ challenge1.sql
@@ -1,8 +1,15 @@
 -- 1. Mencari movie dengan tahun rilis lebih dari tahun 2000
-select * from movies where year > 2000;
+SELECT * FROM movies 
+WHERE year > 2000;
 
 -- 2. Mencari actor dengan akhiran nama 's' 
-select * from actors where last_name ilike '%s'; 
+SELECT * FROM actors 
+WHERE last_name 
+ILIKE '%s'; 
 
 -- 3. Mencari movie dengan rating diantara 5 dan 7 dan tahun rilis 2004 sampai 2006
-select * from movies where (rankscore BETWEEN 5 and 7) and (year BETWEEN 2004 and 2006) ORDER BY rankscore DESC;
+SELECT * FROM movies 
+WHERE (rankscore BETWEEN 5 and 7) and (year BETWEEN 2004 and 2006) 
+ORDER BY rankscore DESC;
+
+
--- challenge2.sql
+++ challenge2.sql
@@ -10,3 +10,21 @@ select a.first_name, a.last_name, r.role, m.name as title from actors a
 join roles r on r.actor_id = a.id
 join movies m on m.id = r.movie_id
 limit 50;
+
+-- 6. Mendapatkan data director, berapa genre yang di-direct
+select d.first_name, d.last_name, count(mg.genre) as genres_count from movies m
+join movies_directors md on md.movie_id = m.id
+join movies_genres mg on mg.movie_id = m.id
+join directors d on d.id = md.director_id 
+GROUP BY d.id;
+-- 7. Mendapatkan data actor yang memiliki roles lebih dari 5
+select * from (select a.first_name, a.last_name, count(r.role) as role_count from actors a
+join roles r on r.actor_id = a.id
+join movies m on m.id = r.movie_id 
+GROUP BY a.id) where role_count > 5;
+select * from actors;
+-- 8. Mendapatkan director paling produktif sepanjang masa
+
+-- 9. Mendapatkan tahun tersibuk sepanjang masa
+
+-- 10. Mendapatkan movies dnegan genres yang dibuatkan menjadi 1 column (value dipisahkan dengan comma) dengan menggunakan string_agg
