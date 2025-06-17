4bbef569f78b7f95b6c0a71a6dad6a34e15f0e4a
yusufbahtiarr <yusufbdev@gmail.com>
2025-06-17 09:57:40 +0700

    init: minitask database query

--- challenge1.sql
+++ challenge1.sql
@@ -1,5 +1,5 @@
 -- 1. Mencari movie dengan tahun rilis lebih dari tahun 2000
-select * from movies where year > 2000
+select * from movies where year > 2000;
 
 -- 2. Mencari actor dengan akhiran nama 's' 
-select * from actors where last_name ilike '%s' 
+select * from actors where last_name ilike '%s'; 
