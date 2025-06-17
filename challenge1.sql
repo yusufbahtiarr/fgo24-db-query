aecd03c61115faf08965a3928683906df3733c9b
yusufbahtiarr <yusufbdev@gmail.com>
2025-06-17 10:05:23 +0700

    feat: add filter movie rating and year

--- challenge1.sql
+++ challenge1.sql
@@ -5,4 +5,4 @@ select * from movies where year > 2000;
 select * from actors where last_name ilike '%s'; 
 
 -- 3. Mencari movie dengan rating diantara 5 dan 7 dan tahun rilis 2004 sampai 2006
-select * from movies where (rankscore BETWEEN 5 and 7) and (year BETWEEN 2004 and 2006);
+select * from movies where (rankscore BETWEEN 5 and 7) and (year BETWEEN 2004 and 2006) ORDER BY rankscore DESC;
