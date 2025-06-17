c49f2fcc3ec363059862659134e9ddbb53366788
yusufbahtiarr <yusufbdev@gmail.com>
2025-06-17 11:16:24 +0700

    feat: add challenge2 task 2

--- challenge2.sql
+++ challenge2.sql
@@ -4,3 +4,9 @@ join movies_directors md on md.movie_id = m.id
 join movies_genres mg on mg.movie_id = m.id
 join directors d on d.id = md.director_id
 limit 50;
+
+-- 5. Melakukan join movie dan roles berdasarkan table actors.
+select a.first_name, a.last_name, r.role, m.name from actors a
+join roles r on r.actor_id = a.id
+join movies m on m.id = r.movie_id
+limit 50;
